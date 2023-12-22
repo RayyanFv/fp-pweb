36d937268509be9ad9358c49310e4f0c557ac5ea
RayyanFv <rayyanfv@gmail.com>
2023-12-22 11:07:06 +0700

    fix: navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { FaInstagram, FaGithub } from 'react-icons/fa';
 
 function Footer() {
-    return (
-        <>
-    <div className='Footer mt-4 mb-10 mx-44'>
-            <div className='Footer1 flex flex-row mb-2'>
-                <div className='font-bold mr-4'>Follow Us</div>
-                <div className='font-bold mx-2'>Foto Instagram</div>
-                <div className='font-bold mx-2'>Foto Github</div>
-            </div>
-            <div className='border-b-2 my-4 w-au'></div>
-            <input class="border-2 border-white py-3 px-4 w-full" placeholder="Send to us an email" name="send" type="text" required></input>
-            <div className='mr-4 my-3' style={{ fontSize: '12px' }}>I accept Google's Terms and Conditions and acknowledge that my information will be used in accordance with Google's Privacy Policy.</div>
-            <div className='border-b-2 my-4 w-au'></div>
-            <div className='Footer1 flex flex-row mb-2'>
-                <div className='font-bold mr-4'>Z-Sharp</div>
-                <div className='font-light mx-2'>Privacy</div>
-                <div className='font-light mx-2'>Terms</div>
-            </div>
-    </div>
-        </>
-      
-    )
-  }
-  
-  export default Footer
+  const [emailSent, setEmailSent] = useState(false);
+
+  const handleSendEmail = () => {
+    setEmailSent(true);
+  };
+
+  return (
+    <>
+      <div className='Footer mt-4 mb-10 mx-44'>
+        <div className='Footer1 flex flex-row mb-2'>
+          <div className='font-bold mr-4'>Follow Us</div>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className='font-bold mx-2'>
+            <FaInstagram size={20} className='h-5 w-5' />
+          </a>
+          <a href="https://github.com/" target="_blank" rel="noopener noreferrer" className='font-bold mx-2'>
+            <FaGithub size={20} className='h-5 w-5' />
+          </a>
+        </div>
+        <div className='border-b-2 my-4 w-au'></div>
+        <div className="flex flex-col">
+          <input
+            className="border-2 border-white py-3 px-4 w-full mb-2"
+            placeholder="Send us an email"
+            name="send"
+            type="text"
+            required
+          />
+          <button
+            className="bg-footer border-2 text-white w-fit py-3 px-4 rounded-full ml-auto"
+            onClick={handleSendEmail}
+          >
+            Send
+          </button>
+          {emailSent && <div className="text-green-500 mt-2">Email sent!</div>}
+        </div>
+        <div className='mr-4 my-3' style={{ fontSize: '12px' }}>
+          I accept Google's Terms and Conditions and acknowledge that my information will be used in accordance with Google's Privacy Policy.
+        </div>
+        <div className='border-b-2 my-4 w-au'></div>
+        <div className='Footer1 flex flex-row mb-2'>
+          <div className='font-bold mr-4'>Z-Sharp</div>
+          <a href="/privacy" className='font-light mx-2'>Privacy</a>
+          <a href="/terms" className='font-light mx-2'>Terms</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,22 @@
-import React from 'react'
-import HeaderItem from './HeaderItem';
+import React from 'react';
+
 
 function Header() {
-    const menu=[
-        {
-            name:'Z-Sharp'
-        },
-        {
-            name:'Technologies'
-        },
-        {
-            name:'Purchase'
-        },
-        {
-            name:'About Us'
-        }
-    ]
   return (
-    <div className='flex flex-row rounded-full border-2 bg-navbar h-fit w-auto mt-12 px-3 mx-44'>
-        <div className='font-normal flex justify-left flex-row gap-8 mt-4 ml-8 mb-4'>
-        {menu.map((item)=>(
-            <HeaderItem name={item.name}/>
-        ))}
+    <div className="App">
+      <header className="className='flex rounded-full border-2 mt-12 mx-44 bg-navbar h-auto w-auto'">
+        <div className="container mx-auto flex items-center justify-between gap-8 py-2">
+          <a href="/" className="text-white ml-10 font-bold text-xl">Z-Sharp</a>
+          <a href="/about" className="text-white">Technologies</a>
+          <a href="/services" className="text-white">Purchase</a>
+          <a href="/contact" className="text-white">About</a>
+          <div className="space-x-4 ml-auto mr-7 rounded-full bg-white py-2 px-4">
+          <a href="/contact" className="text-black font-bold">Sign In</a>
+          </div>
         </div>
-        <div className='flex flex-row rounded-full justify-end border-2 bg-semiwhite h-auto mx-6 w-auto px-4'>
-        <div className='font-bold text-main flex justify-end flex-row gap-8 mt-4 mx-8 mb-4'>wayao</div>
-        </div>
-        
+      </header>
     </div>
-  )
+  );
 }
-export default Header
+
+export default Header;
