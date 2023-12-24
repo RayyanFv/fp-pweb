6e091cfefa86d807a9be21318ecc3ae835a75713
Moch. Avin <5025221061@mhs.its.ac.id>
2023-12-24 18:18:55 +0700

    fix: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
-
+import Dropdown from './Dropdown';
 
 function Header() {
-
   return (
     <div className='bg-[#0C132F] py-10'>
-    <div className="className='flex rounded-full border-2 mx-44 bg-navbar h-auto w-auto'">
-        <div className="container mx-auto flex items-center justify-between gap-8 py-2">
-          <a href="/" className="text-white ml-10 font-bold text-xl">Z-Sharp</a>
-          <a href="/technologies" className="text-white">Technologies</a>
-          <a href="/purchase" className="text-white">Purchase</a>
-          <a href="/about-us" className="text-white">About</a>
-          <div className="space-x-4 ml-auto mr-7 rounded-full bg-white py-2 px-4">
-          <a href="/signin" className="text-black font-bold">User</a>
-          </div>
+      <div className="flex rounded-full border-2 lg:mx-44 md:mx-16 mx-8 bg-navbar h-auto w-auto container items-center justify-between py-2">
+        <div className='flex text-white gap-8 ml-8 item-center'>
+          <a href="/" className="font-bold text-xl hover:scale-110 transition-all">Z-Sharp</a>
+          <a href="/technologies" className='mt-[3px]'>Technologies</a>
+          <a href="/purchase" className='mt-[3px]'>Purchase</a>
+          <a href="/about-us" className='mt-[3px]'>About</a>
+        </div>
+        <div>
+          <Dropdown />
         </div>
       </div>
     </div>
-    
-   
+
+
   );
 }
 
